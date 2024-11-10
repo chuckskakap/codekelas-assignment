8111fdbe78f7636d018f5994473dbb22b1f02960
Aiman Adam <80781549+chuckskakap@users.noreply.github.com>
2024-11-10 21:29:01 +0800

    Add files via upload

---
+++ Java/codekelasjava/src/CodeKelas.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CodeKelas {
+    public static void main(String[] args) {
+
+        double length = 15.99984747;
+
+        length = "syenddf";
+        System.out.printf("%10.3f", length);
+
+        // double result = (double) 10 / 4;
+        // System.out.println(result);
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Car price: ");
+        // int carPrice = scanner.nextInt();
+        // System.out.print("Interest rate: ");
+        // float interestRate = scanner.nextFloat();
+        // System.out.print("Downpayment: ");
+        // int downPayment = scanner.nextInt();
+        // System.out.print("Period (year/s): ");
+        // int period = scanner.nextInt();
+
+        // final byte MONTHS_IN_YEAR = 12;
+        // final byte PERCENT = 100;
+
+        // float totalLoan = carPrice + (interestRate / PERCENT) * carPrice;
+        // float monthlyPayment = totalLoan / (period * MONTHS_IN_YEAR);
+
+        // System.out.println("Your Total Loan is " + Math.round(totalLoan));
+        // System.out.println("Your Monthly Payment is " + Math.round(monthlyPayment));
+
+    }
+}
